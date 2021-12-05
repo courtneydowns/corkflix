e039dc3db52432957a4b1c2601966af3a9072510
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-12-04 20:38:59 -0500

    beginning of validation

--- register.php
+++ register.php
@@ -1,37 +1,67 @@
 <?php
+require_once("includes/classes/FormSanitizer.php");
+
     if(isset($_POST["submitButton"])) {
-        echo "Form was submitted";
+        
+        $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
+        $lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        $email = FormSanitizer::sanitizeFormEmail($_POST["email"]);
+        $email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
+        $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+        $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
+
+        echo $firstName;
+        echo $lastName;
+        echo $email;
+        echo $email2;
+        echo $password;
+        echo $password2;
     }
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Welcome to CorkFlix</title>
-    <link rel="stylesheet" type="text/css" href="./assets/style/style.css" />
-</head>
+    <head>
+        <title>Welcome to Corkflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+    </head>
     <body>
-    
-<div class="signInContainer">
-    <div class=column>
-        <div class="header">
-            <h2>Sign Up</h2>
-            <span>to continue to CorkFlix</span>
-        </div>
-        <form method="POST">
-            <input type="text" name="firstName" placeholder="First Name" required/>
-            <input type="text" name="lastName" placeholder="Last Name" required/>
-            <input type="text" name="username" placeholder="Username" required/>
-            <input type="email" name="email" placeholder="Email" required/>
-            <input type="email" name="email2" placeholder="Confirm Email" required/>
-            <input type="password" name="password" placeholder="Password" required/>
-            <input type="password" name="password2" placeholder="Confirm password" required/>
-            <input type="submit" name="submitButton" value="SUBMIT"/>
-        </form>
-    </div>
+        
+        <div class="signInContainer">
 
-</div>
+            <div class="column">
+
+                <div class="header">
+                    <img src="assets/images/logo.png" title="Logo" alt="Site logo" />
+                    <h3>Sign Up</h3>
+                    <span>to continue to Corkflix</span>
+                </div>
+
+                <form method="POST">
+
+                    <input type="text" name="firstName" placeholder="First name" required>
+
+                    <input type="text" name="lastName" placeholder="Last name" required>
+
+                    <input type="text" name="username" placeholder="Username" required>
+
+                    <input type="email" name="email" placeholder="Email" required>
+
+                    <input type="email" name="email2" placeholder="Confirm email" required>
+
+                    <input type="password" name="password" placeholder="Password" required>
+
+                    <input type="password" name="password2" placeholder="Confirm password" required>
+
+                    <input type="submit" name="submitButton" value="SUBMIT">
+
+                </form>
+
+                <a href="login.php" class="signInMessage">Already have an account? Sign in here!</a>
+
+            </div>
+
+        </div>
 
     </body>
-
-
 </html>
