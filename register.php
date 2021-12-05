7def02ab4071ce26e60f6e679d0c89e71b040c70
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-12-04 19:29:24 -0500

    the beginning of the project

---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+    if(isset($_POST["submitButton"])) {
+        echo "Form was submitted";
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to CorkFlix</title>
+    <link rel="stylesheet" type="text/css" href="./assets/style/style.css" />
+</head>
+    <body>
+    
+<div class="signInContainer">
+    <div class=column>
+        <div class="header">
+            <h2>Sign Up</h2>
+            <span>to continue to CorkFlix</span>
+        </div>
+        <form method="POST">
+            <input type="text" name="firstName" placeholder="First Name" required/>
+            <input type="text" name="lastName" placeholder="Last Name" required/>
+            <input type="text" name="username" placeholder="Username" required/>
+            <input type="email" name="email" placeholder="Email" required/>
+            <input type="email" name="email2" placeholder="Confirm Email" required/>
+            <input type="password" name="password" placeholder="Password" required/>
+            <input type="password" name="password2" placeholder="Confirm password" required/>
+            <input type="submit" name="submitButton" value="SUBMIT"/>
+        </form>
+    </div>
+
+</div>
+
+    </body>
+
+
+</html>
